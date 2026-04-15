bf7782156cc15d47ead50345ec067a78d79152a0
Jhon Appleseed <global.appleseed@gmail.com>
2026-04-15 11:19:42 +0300

    Add CORS origin for production environment

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,8 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://localhost:8000",
-    "selfless-blessing-production-fd99.up.railway.app"
+    "selfless-blessing-production-fd99.up.railway.app",
+    "https://noteb.up.railway.app"
     ]
 
 app.add_middleware(
